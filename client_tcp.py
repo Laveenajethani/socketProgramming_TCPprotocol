8777187c3c0f8c15e4f62c4039e314d43ffad5d0
Laveena Jethani <laveena.j.21@gmail.com>
2021-01-17 12:55:38 +0530

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -4,13 +4,13 @@ server_IP=input("Enter Server IP: ")
 server_port=int(input("Enter server port: "))
 s.connect((server_IP,server_port))
 while True:
-    send_msg=input("What msg you want to send to server {}: ".format(server_IP))
+    send_msg=input("What msg you want send on the server {}: ".format(server_IP))
     if(send_msg=='exit'):
         s.send(b"exit")
         s.close()
         break
     send_msg=send_msg.encode()
     s.send(send_msg)
-    reply_server=s.recv(120)
+    reply_server=s.recv(1024)
     reply_server=reply_server.decode()
     print("msg received from the server"+": "+reply_server)
